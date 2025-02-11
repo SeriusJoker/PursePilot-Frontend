af744637bf394c4d635c3b5aef4c82d35d6a1fef
Serius_Joker <alexintka1@gmail.com>
2025-02-12 10:26:16 +1300

    Tweaking basename for index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ Import React Router
+import { HashRouter } from 'react-router-dom'; // ✅ Use HashRouter instead
 import './index.css';
-import App from './App.js';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
